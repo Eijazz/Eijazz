3e6b4f9b3b4311a143445fc44110c2fccf16fedc
Eijazz <149861794+Eijazz@users.noreply.github.com>
2024-10-19 14:07:17 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1729321479,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/eijaznitip-sheet0.png",
+		"images/stargold-sheet0.png",
+		"images/tilegreen-sheet0.png",
+		"images/playerred_walk-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
